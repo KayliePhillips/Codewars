b5c50254d7e40bf46643b37c142b5ed22b2a10ef
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-16 16:50:06 -0500

    BasicMathematicalOperations

--- Codewars/Methods.cs
+++ Codewars/Methods.cs
@@ -54,7 +54,24 @@ namespace Codewars
                 default:
                     return "Beer";
                     break;
-            }   
+            }
         }
+        //BasicMathematicalOperations
+        //Best solution
+        public static double basicOp(char op, double val1, double val2)
+        { 
+                switch (op)
+                { 
+                    case '+': return val1 + val2;
+                    case '-': return val1 - val2;
+                    case '*': return val1 * val2;
+                    case '/': return val1 / val2;
+                    default:
+                        throw new System.ArgumentException("Unknown operation!", op.ToString());
+                }
+        }
+                   
+        
+
     }
 }
--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Codewars;
+
+Console.WriteLine(Methods.basicOp('+', 4, 3));
