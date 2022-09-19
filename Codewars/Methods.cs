9120b585409e8665a6e722fab32b54286718ac6d
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-19 18:37:01 -0500

    Completed more codewars

--- Codewars/Methods.cs
+++ Codewars/Methods.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,8 @@ namespace Codewars
 {
     internal class Methods
     {
-        //EvenOrOdd
+        #region All Completed Codewars Methods
+        #region Codewars: Even Or Odd - Given input it returns if the number is even or odd.
         public static string EvenOrOdd(int number)
         {
             if (number % 2 == 0)
@@ -20,15 +22,16 @@ namespace Codewars
                 return "Odd";
             }
         }
+        #endregion
 
-        //Return String
+        #region Codewars: Returning Strings - Given an input it returns the input in a written message.  
         public static string Greet(string name)
         {
             return ($"Hello, {name} how are you doing today?");
         }
+        #endregion
 
-
-        //Bartender, drink!
+        #region Codewars: L1:Bartender, drink! - Switch that reads a name and returns what drink that person likes
         public static string GetDrinkByProfession(string p)
         {
             switch (p.ToLower())
@@ -56,22 +59,83 @@ namespace Codewars
                     break;
             }
         }
-        //BasicMathematicalOperations
-        //Best solution
+        #endregion
+
+        #region Codewars: Basic Mathematical Operations - reads input of an operator, two values and then completes the equations with the input
         public static double basicOp(char op, double val1, double val2)
         { 
-                switch (op)
-                { 
-                    case '+': return val1 + val2;
-                    case '-': return val1 - val2;
-                    case '*': return val1 * val2;
-                    case '/': return val1 / val2;
-                    default:
-                        throw new System.ArgumentException("Unknown operation!", op.ToString());
-                }
+            switch (op)
+            { 
+                case '+': return val1 + val2;
+                case '-': return val1 - val2;
+                case '*': return val1 * val2;
+                case '/': return val1 / val2;
+                default:
+                    throw new System.ArgumentException("Unknown operation!", op.ToString());
+            }
         }
-                   
-        
+        #endregion
 
+        #region Codewars: Opposite Number - Return Opposite Number.  Ex. Give 1, return -1
+        public static int Opposite(int number)
+        {
+            return -number;
+        }
+        #endregion
+
+        #region Codewars: Make Upper Case - Given a string, return it in All Uper Case Ex. Given John, return JOHN
+        public static string MakeUpperCase(string str)
+        {
+            return str.ToUpper();
+        }
+        #endregion
+
+        #region Codewars: Find the remainder
+        public static int Remainder(int a, int b)
+        {
+            if (a > b)
+            {
+                return a % b;
+            }
+            else
+            {
+                return b % a;
+            }
+
+        }
+        #endregion
+
+        #region Codewars: Sentance Smash - Take individual words and put them into one sentence.  
+        public static string Smash(string[] words)
+        {
+            return String.Join(" ", words);
+        }
+        #endregion
+
+        #region  Codewars: Grasshopper -Summation --Write a program that finds the summation of every number from 1 to num.       
+        public static int summation(int num)
+        {
+            int sum = 0;
+            for (int i = 0; i <= num; i++)
+            {
+                sum += i; //sum = sum + i
+            }
+            return sum;
+        }
+
+        #endregion
+
+       
+        #region Codewars: Opposites Attract -- Pick flowers peters to determine love
+        public static bool lovefunc(int flower1, int flower2)
+        {
+
+            if (flower1 % 2 == 0 && flower2 % 2 != 0) return true;
+            if (flower1 % 2 != 0 && flower2 % 2 == 0) return true;
+            return false;
+        }
+        #endregion
+        #endregion All Completed Codewars Methods
     }
+
 }
--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -1,3 +1,33 @@
 ﻿using Codewars;
+using System;
 
-Console.WriteLine(Methods.basicOp('+', 4, 3));
+#region All Completed Codewars 
+
+#region: Codewars: Basic Mathematical Operations - reads input of an operator, two values and then completes the equations with the input
+//Console.WriteLine(Methods.basicOp('+', 4, 3));
+#endregion
+
+#region Codewars: Opposite Number - Return Opposite Number.  Ex. Give 1, return -1
+//Console.WriteLine(Methods.Opposite(3));
+#endregion
+
+#region Codewars: Find the Remainder
+//Console.WriteLine(Methods.Remainder(0, 1));
+#endregion
+
+#region Codewars: Sentence Smash
+//string[] givenWords = new string[] { "hi", "how", "are", "you" };
+//Console.WriteLine(Methods.Smash(givenWords));
+#endregion
+
+#region Codewars: Grasshopper -Summation --Write a program that finds the summation of every number from 1 to num.
+//Console.WriteLine(Methods.summation(4));
+#endregion
+
+#region Codewars: Opposites Attract --Pick flowers peters to determine love
+Console.WriteLine(Methods.lovefunc(0,0));
+Console.WriteLine(Methods.lovefunc(2, 2));
+Console.WriteLine(Methods.lovefunc(3, 2));
+#endregion
+
+#endregion All of Completed Codewars
